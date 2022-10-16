e8229a2b43c0f3ed3d2d500d5592b398d7088a7b
12ARSEN1 <115397119+12ARSEN1@users.noreply.github.com>
2022-10-16 13:05:07 +0300

    Update game.py

--- game/game.py
+++ game/game.py
@@ -18,3 +18,19 @@ window = display.set_mode((win_height,win_width))
 BLUE = (200,255,255)
 window.fill(BLUE)
 
+# прапорці, які відповідають за стан гри
+game = True
+finish = False
+
+clock = time.Clock() # годинник 
+FPS = 60 #кількість кадрів в секунду
+
+# ігровий цикл
+while game:
+    for e in event.get():  # перевірка всіх подій
+        if e.type == QUIT: # тип подій - закрити вікно
+            game = False   # закінчуємо цикл while
+    if finish != True:
+        window.fill(fon)
+    display.update()
+    clock.tick(FPS)
